0a4578c04240d13094e939ca5e5eb8e370370c66
stitihaythem <stiti.haythem@protonmail.com>
2024-08-15 12:57:28 +0200

    Initialized Spring Boot project

---
+++ src/main/java/org/aid/letsgettogether/LetsGetTogetherApplication.java
@@ -0,0 +1,13 @@
+package org.aid.letsgettogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsGetTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsGetTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aid/letsgettogether/LetsGetTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aid.letsgettogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsGetTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
